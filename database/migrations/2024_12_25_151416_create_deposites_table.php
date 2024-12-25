124ae756666e20d9182830afdc1d64a19021b508
mstsurnalyakter <surnalyms@gmail.com>
2024-12-25 08:26:08 -0700

    add fields to deposites table for amount, user_id, transaction_id, payment method, payment number, and status

--- database/migrations/2024_12_25_151416_create_deposites_table.php
+++ database/migrations/2024_12_25_151416_create_deposites_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('deposites', function (Blueprint $table) {
             $table->id();
+            $table->string('amount');
+            $table->string('user_id');
+            $table->string('transaction_id');
+            $table->string('paymentMethod');
+            $table->string('aymentNumber');
+            $table->string('status')->default('pending');
             $table->timestamps();
         });
     }
